44ada1c4217161daafafd359b9ff811128a1c8a2
mirr254 <sammysteppa90@gmail.com>
2019-08-13 09:06:23 +0300

    fix(entrypoint) base64 command in alpine

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -6,14 +6,14 @@ if [ ! -f ingress.crt ]; then
     kubectl get secret gluu -o json \
     | grep '\"ssl_cert' \
     | awk -F '"' '{print $4}' \
-    | base64 --decode > ingress.crt
+    | base64 -d > ingress.crt
 fi
 
 if [ ! -f ingress.key ]; then
     kubectl get secret gluu -o json \
     | grep '\"ssl_key' \
     | awk -F '"' '{print $4}' \
-    | base64 --decode > ingress.key
+    | base64 -d > ingress.key
 fi
 
 kubectl create secret tls tls-certificate --key ingress.key --cert ingress.crt
